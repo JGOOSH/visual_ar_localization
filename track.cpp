e34ec4f959dfe5d4c56f331fabebbd716a0b5630
patel1.shivam2@gmail.com <patel1.shivam2@gmail.com>
2017-10-31 16:12:33 -0500

    initial

--- track.cpp
+++ track.cpp
@@ -8,34 +8,34 @@ bool markerSeen = false;
 //HashSet K,V --> ID, Location
 
 visualization_msgs::Marker current_vis_msg;
+// geometry_msgs::Pose current_pose;
+// geometry_msgs::Vector3 pose_direction;
 
 void vis_cb(const visualization_msgs::Marker::ConstPtr& msg) {
-    current_pose = *msg;
+    current_vis_msg = *msg;
     markerSeen = true;
 }
 
 int main(int argc, char **argv){
     
     //initialize the node
-    ros::init(argc, argv, "traveling_salesturtle");
+    ros::init(argc, argv, "ar_tag_tracker");
     
     //instantiate the node handle which is used for creating publishers and subscribers
     ros::NodeHandle n;
 
     //subscriber for the /visualization_marker
-    ros::Subscriber vis_sub = n.subscribe("/visualization_marker", 1, vis_cb );
+    ros::Subscriber vis_sub = n.subscribe("/visualization_marker", 1, vis_cb);
 
     //your code for solving the traveling salesmane problem goes here (of course you can define helper functions above the main function)
     while(ros::ok()) {
     	ros::spinOnce();
+        //CREATE DS that holds all the tags and their positions
     	if(markerSeen)
     	{
-    		for(int count = 0; count < sizeof(x_coords)/sizeof(x_coords[0]); count++)
-    		{
-    			goToLoc(x_coords[count], y_coords[count]);
-    		}
+            ROS_INFO("Current tag is at x : %f, y : %f, z : %f", current_vis_msg.pose.position.x, current_vis_msg.pose.position.y
+                , current_vis_msg.pose.position.z);
     		markerSeen = false;
-    		break;
     	}
 	}
 }
