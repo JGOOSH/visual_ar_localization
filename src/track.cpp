420d28c0ca1471a7eaf25c3c6d1b08766fc14219
JGOOSH <jgoo@utexas.edu>
2017-11-09 16:08:42 -0600

    basic data structure implemented

--- src/track.cpp
+++ src/track.cpp
@@ -2,14 +2,11 @@
 #include "std_msgs/String.h"
 #include <visualization_msgs/Marker.h>
 #include "geometry_msgs/Twist.h"
+#include <map>
 
 bool markerSeen = false;
 
-//HashSet K,V --> ID, Location
-
 visualization_msgs::Marker current_vis_msg;
-// geometry_msgs::Pose current_pose;
-// geometry_msgs::Vector3 pose_direction;
 
 void vis_cb(const visualization_msgs::Marker::ConstPtr& msg) {
     current_vis_msg = *msg;
@@ -27,14 +24,24 @@ int main(int argc, char **argv){
     //subscriber for the /visualization_marker
     ros::Subscriber vis_sub = n.subscribe("/visualization_marker", 1, vis_cb);
 
+    std::map<int, geometry_msgs::Pose> pose_map;
+    std::map<int, geometry_msgs::Pose>::iterator it;
+
     //your code for solving the traveling salesmane problem goes here (of course you can define helper functions above the main function)
     while(ros::ok()) {
     	ros::spinOnce();
         //CREATE DS that holds all the tags and their positions
     	if(markerSeen)
     	{
-            ROS_INFO("Current tag is at x : %f, y : %f, z : %f", current_vis_msg.pose.position.x, current_vis_msg.pose.position.y
-                , current_vis_msg.pose.position.z);
+        it = pose_map.find(current_vis_msg.id);
+        if(it == pose_map.end()){
+          ROS_INFO("Current tag is at x : %f, y : %f, z : %f", current_vis_msg.pose.position.x, current_vis_msg.pose.position.y
+          , current_vis_msg.pose.position.z);
+          pose_map.insert(it, std::pair<int, geometry_msgs::Pose>(current_vis_msg.id , current_vis_msg.pose));
+        }
+        else {
+          ROS_INFO("ALREADY EXISTS"); 
+        }
     		markerSeen = false;
     	}
 	}
