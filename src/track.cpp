e6f108f213b04c373cf3c4601572e93693c5cf26
patel1.shivam2@gmail.com <patel1.shivam2@gmail.com>
2017-11-11 13:46:50 -0600

    with transform ar-map

--- src/track.cpp
+++ src/track.cpp
@@ -24,25 +24,41 @@ int main(int argc, char **argv){
     //subscriber for the /visualization_marker
     ros::Subscriber vis_sub = n.subscribe("/visualization_marker", 1, vis_cb);
 
+    //HOLDS ID->Pose pairs
     std::map<int, geometry_msgs::Pose> pose_map;
+    //Iterator of the map to find if an ID already exists when detecting a new Tag
     std::map<int, geometry_msgs::Pose>::iterator it;
 
-    //your code for solving the traveling salesmane problem goes here (of course you can define helper functions above the main function)
+    //Pose of Tag with resp to map
+    geometry_msgs::Pose tag_wresp_map;
+
     while(ros::ok()) {
     	ros::spinOnce();
-        //CREATE DS that holds all the tags and their positions
-    	if(markerSeen)
-    	{
-        it = pose_map.find(current_vis_msg.id);
-        if(it == pose_map.end()){
-          ROS_INFO("Current tag is at x : %f, y : %f, z : %f", current_vis_msg.pose.position.x, current_vis_msg.pose.position.y
-          , current_vis_msg.pose.position.z);
-          pose_map.insert(it, std::pair<int, geometry_msgs::Pose>(current_vis_msg.id , current_vis_msg.pose));
-        }
-        else {
-          ROS_INFO("ALREADY EXISTS"); 
-        }
-    		markerSeen = false;
-    	}
+    	if(markerSeen) {
+	        it = pose_map.find(current_vis_msg.id);
+	        if(it == pose_map.end()) {
+	        	try{
+	        		ROS_INFO_STREAM("wait");
+	        		tf_l.waitForTransform(base_link,
+	                              current_vis_msg.pose, ros::Time(0), ros::Duration(1));
+	        		ROS_INFO_STREAM("before transform");
+	        		tf_l.transformPose(base_link, current_vis_msg.pose, tag_wresp_map);
+	        		ROS_INFO_STREAM("Transorm exected");
+	        		//JAMIN TAKES tag_wresp_map and annotates the map with it
+
+			    }
+			    catch (tf::TransformException ex){
+			        ROS_ERROR("%s",ex.what());
+			    }
+	          ROS_INFO("Current tag is at x : %f, y : %f, z : %f", current_vis_msg.pose.position.x, current_vis_msg.pose.position.y
+	          , current_vis_msg.pose.position.z);
+	          pose_map.insert(it, std::pair<int, geometry_msgs::Pose>(current_vis_msg.id , current_vis_msg.pose));
+
+	        }
+	        else {
+	          ROS_INFO("ALREADY EXISTS"); 
+	        }
+	    		markerSeen = false;
+	    }
 	}
 }
