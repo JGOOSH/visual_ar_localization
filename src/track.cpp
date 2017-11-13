7aef1c6beaf1fe863590dc989ae4c3056b8abffb
Jamin Goo <jgoo@utexas.edu>
2017-11-12 21:56:32 -0600

    started adding publisher

--- src/track.cpp
+++ src/track.cpp
@@ -29,6 +29,9 @@ int main(int argc, char **argv){
     //subscriber for the /visualization_marker
     ros::Subscriber vis_sub = n.subscribe("/visualization_marker", 1, vis_cb);
 
+    //publisher for the markers on the map
+    ros::Publisher marker_pub = n.advertise<visualization_msgs::Makrer>("map_markers", 1000);
+
     //HOLDS ID->Pose pairs
     std::map<int, geometry_msgs::PoseStamped> pose_map;
     //Iterator of the map to find if an ID already exists when detecting a new Tag
@@ -37,34 +40,35 @@ int main(int argc, char **argv){
     //input stampedPose
    	geometry_msgs::PoseStamped stampedPose;
 
-
     //output stampedPose
     geometry_msgs::PoseStamped tag_wresp_map;
 
     while(ros::ok()) {
     	ros::spinOnce();
+      visualization_msgs::Marker map_msg;
     	if(markerSeen) {
     		//Iterator stores the location of the id within the internal map
-	        it = pose_map.find(current_vis_msg.id);
-	        //If the tag is NEW
-	        if(it == pose_map.end()) {
-	        	//Transfer information into a StampedPose
-				stampedPose.header = current_vis_msg.header;
-				stampedPose.pose = current_vis_msg.pose;
-	        	try{
-	        		tf_l.waitForTransform("/map",
-	                              stampedPose.header.frame_id, ros::Time(0), ros::Duration(1));
-	        		//Performs transformation and stores it in the third parameter
-	        		tf_l.transformPose("/map", stampedPose, tag_wresp_map);
-	        		//JAMIN TAKES tag_wresp_map and annotates the map with its
-			    }
+        it = pose_map.find(current_vis_msg.id);
+        //If the tag is NEW
+        if(it == pose_map.end()) {
+      	  //Transfer information into a StampedPose
+          msgs = current_vis_msg;
+  				stampedPose.header = current_vis_msg.header;
+  				stampedPose.pose = current_vis_msg.pose;
+        	try{
+        		tf_l.waitForTransform("/map",
+                              stampedPose.header.frame_id, ros::Time(0), ros::Duration(1));
+        		//Performs transformation and stores it in the third parameter
+        		tf_l.transformPose("/map", stampedPose, tag_wresp_map);
+        		//JAMIN TAKES tag_wresp_map and annotates the map with its
+		         }
 			    catch (tf::TransformException ex){
 			        ROS_ERROR("TransformException");
-			    }
-	          ROS_INFO("Current tag is at x : %f, y : %f, z : %f", tag_wresp_map.pose.position.x, tag_wresp_map.pose.position.y
-	          , tag_wresp_map.pose.position.z);
-	          pose_map.insert(it, std::pair<int, geometry_msgs::PoseStamped>(current_vis_msg.id , tag_wresp_map));
-
+			         }
+          ROS_INFO("Current tag is at x : %f, y : %f, z : %f", tag_wresp_map.pose.position.x, tag_wresp_map.pose.position.y
+          , tag_wresp_map.pose.position.z);
+          pose_map.insert(it, std::pair<int, geometry_msgs::PoseStamped>(current_vis_msg.id , tag_wresp_map));
+          marker_pub.publish(map_msg);
 	        }
 	        else {
 	        	//The issue here is that we have to use the Pose that the Iterator is pointing at!!
