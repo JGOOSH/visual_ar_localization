603d533f3b37cf4b5dac19be2b75ea4e3ad60fd2
Shivam <patel1.shivam2@gmail.com>
2017-11-17 13:06:45 -0600

    quat type

--- src/track.cpp
+++ src/track.cpp
@@ -61,18 +61,18 @@ int main(int argc, char **argv){
 				pQuat.z() = current_vis_msg.pose.position.z;
 				pQuat.w() = 0;
 
-				Quaternion arPose_wrt_robot = current_vis_msg.pose.orientation.inverse();
+				Quaternionf arPose_wrt_robot = current_vis_msg.pose.orientation.inverse();
 				arPose_wrt_robot*= pQuat;
 				arPose_wrt_robot*= current_vis_msg.pose.orientation;
 
 				geometry_msgs::Pose curPose = pose_map.at(current_vis_msg.id).pose;
 				Quaternion pARTag = new Quaternion(curPose.position.x, curPose.position.y, curPose.pose.position.z, 0);
 				
-				Quaternion arPose_wrt_map = curPose.orientation.inverse();
+				Quaternionf arPose_wrt_map = curPose.orientation.inverse();
 				arPose_wrt_map*= pARTag;
 				arPose_wrt_map*= curPose.orientation;
 
-		    	Quaternion result = arPose_wrt_robot - arPose_wrt_map;
+		    	Quaternionf result = arPose_wrt_robot - arPose_wrt_map;
 		    	//Grab xyz of result, just do result.x ....
 
 		    	geometry_msgs::Pose outputPose;
