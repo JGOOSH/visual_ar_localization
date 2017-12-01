b42c0902b5374903dcf35be753973ec59f09e216
Shivam <patel1.shivam2@gmail.com>
2017-12-01 17:49:17 -0600

    chain

--- src/track.cpp
+++ src/track.cpp
@@ -14,7 +14,6 @@
 #include <fstream>
 
 bool markerSeen = false;
-bool firstSeen = false;
 
 visualization_msgs::Marker current_vis_msg;
 visualization_msgs::MarkerArray marker_array_msg;
@@ -48,15 +47,9 @@ int main(int argc, char **argv){
     while(ros::ok()) {
     	ros::spinOnce();
     	if(markerSeen) {
-	      	  	//Iterator stores the location of the id within the internal map
-	        it = pose_map.find(current_vis_msg.id);
-	        //If the tag is NEW
-	        if(it == pose_map.end()) {
-	          //first visualize it it in to the rviz
-	          marker_array_msg.markers.push_back(current_vis_msg);
-	      	  //Transfer information into a StampedPose
-	  				stampedPose.header = current_vis_msg.header;
-	  				stampedPose.pose = current_vis_msg.pose;
+	      	  	//Transfer information into a StampedPose
+	  			stampedPose.header = current_vis_msg.header;
+	  			stampedPose.pose = current_vis_msg.pose;
 	        	try {
 	        		tf_l.waitForTransform("/map",
 	                              stampedPose.header.frame_id, ros::Time(0), ros::Duration(1));
@@ -67,65 +60,8 @@ int main(int argc, char **argv){
 			    catch (tf::TransformException ex) {
 			        ROS_ERROR("TransformException");
 			    }
-	          ROS_INFO("Current tag is at x : %f, y : %f, z : %f", tag_wresp_map.pose.position.x, tag_wresp_map.pose.position.y
+	          ROS_INFO("%d, %f, %f, %f", current_vis_msg.id, tag_wresp_map.pose.position.x, tag_wresp_map.pose.position.y
 	          , tag_wresp_map.pose.position.z);
-	          pose_map.insert(it, std::pair<int, geometry_msgs::PoseStamped>(current_vis_msg.id , tag_wresp_map));
-		        }		    	
-	        else {
-		    	Eigen::Quaternionf pQuat;
- 				pQuat.x() = current_vis_msg.pose.position.x;
- 				pQuat.y() = current_vis_msg.pose.position.y;
- 				pQuat.z() = current_vis_msg.pose.position.z;
- 				pQuat.w() = 0;
- 
- 
- 				Eigen::Quaternionf tempQuat;
- 				tempQuat.x() = current_vis_msg.pose.orientation.x;
- 				tempQuat.y() = current_vis_msg.pose.orientation.y;
- 				tempQuat.z() = current_vis_msg.pose.orientation.z;
- 				tempQuat.w() = current_vis_msg.pose.orientation.w;
- 
- 				//This quaternion stores the xyz of the arTag with respect to the robot.
- 				//Got to this quaternion through basic computation from the cur_vis_msg
- 				Eigen::Quaternionf arPose_wrt_robot = tempQuat.inverse();
- 				arPose_wrt_robot*= pQuat;
- 				arPose_wrt_robot*= tempQuat;
- 
- 				geometry_msgs::Pose curPose = pose_map.at(current_vis_msg.id).pose;
- 				Eigen::Quaternionf pARTag;
- 				pARTag.x() = curPose.position.x;
- 				pARTag.y() = curPose.position.y;
- 				pARTag.z() = curPose.position.z;
- 				pARTag.w() = 0;
- 
- 				Eigen::Quaternionf tempQuat2;
- 				tempQuat2.x() = curPose.orientation.x;
-				tempQuat2.y() = curPose.orientation.y;
- 				tempQuat2.z() = curPose.orientation.z;
- 				tempQuat2.w() = curPose.orientation.w;
- 
- 				//This quaternion stores the xyz of the arTag with respect to the map.
- 				//Got to this quaternion through basic computation from the Pose stored in the internal Map
- 				Eigen::Quaternionf arPose_wrt_map = tempQuat2.inverse();
- 				arPose_wrt_map*= pARTag;
- 				arPose_wrt_map*= tempQuat2;
- 
- 		    	/*
- 		    	geometry_msgs::Pose outputPose;
-		    	outputPose.position.x = arPose_wrt_robot.x - arPose_wrt_map.x;
- 		    	outputPose.position.y = arPose_wrt_robot.y - arPose_wrt_map.y;
- 		    	outputPose.position.z = arPose_wrt_robot.z - arPose_wrt_map.z;
- 
- 		    	outputPose.orientation.x = 0;
- 		    	outputPose.orientation.y = 0;
- 		    	outputPose.orientation.z = 0;
- 		    	outputPose.orientation.w = 1;
- 		    	*/
- 		    
- 		    	ROS_INFO("The robot is at x: %f, y : %f, z : %f", arPose_wrt_robot.x() - arPose_wrt_map.x(), arPose_wrt_robot.y() - arPose_wrt_map.y(), arPose_wrt_robot.z() - arPose_wrt_map.z());	
-			}
-	    	markerSeen = false;
-        	marker_pub.publish(marker_array_msg);
 	    }
 	}
 }
