490ffa3c079cfa88efb7ff48dd1cf78ab6af36b0
JGOOSH <jgoo@utexas.edu>
2017-12-02 17:42:03 -0600

    implemented the main logic behind mapping. need to debug

--- src/chain.cpp
+++ src/chain.cpp
@@ -15,10 +15,11 @@
 
 bool marker_seen = false;
 bool first_seen = false;
+bool look_prev = false;
 geometry_msgs::Pose first_pose = NULL;
 visualization_msgs::Marker current_vis_msg;
 Eigen::Matrix4f mats_arr[50];
-int next_tag_id = 0;
+int cur_tag_id = 0;
 
 void vis_cb(const visualization_msgs::Marker::ConstPtr& msg) {
     current_vis_msg = *msg;
@@ -26,20 +27,19 @@ void vis_cb(const visualization_msgs::Marker::ConstPtr& msg) {
 }
 
 Eigen::Matrix4f getMatFromPose() {
-	Eigen::Matrix4f temp;
-	temp.block(0, 0, 2, 2) = current_vis_msg.pose.orientation.toRotationMatrix();
-	temp(0, 3) = current_vis_msg.pose.position.x;
-	temp(1, 3) = current_vis_msg.pose.position.y;
-	temp(2, 3) = current_vis_msg.pose.position.z;
-	temp(3, 3) = 1;
-	return temp;
+  Eigen::Matrix4f temp;
+  temp.block(0, 0, 2, 2) = current_vis_msg.pose.orientation.toRotationMatrix();
+  temp(0, 3) = current_vis_msg.pose.position.x;
+  temp(1, 3) = current_vis_msg.pose.position.y;
+  temp(2, 3) = current_vis_msg.pose.position.z;
+  temp(3, 3) = 1;
+  return temp;
 }
 
 
 int main(int argc, char **argv) {
-
     ros::init(argc, argv, "chain");
-	ros::NodeHandle n;
+	  ros::NodeHandle n;
     //tf::TransformListener tf_l;
 
     //subscriber for the /visualization_marker
@@ -48,18 +48,38 @@ int main(int argc, char **argv) {
     while(ros::ok()) {
     	ros::spinOnce();
     	if(marker_seen) {
-    		if(current_vis_msg.id == next_tag_id) {
-	    		if(!first_seen) {
+    		if(current_vis_msg.id == cur_tag_id) {
+	    		if(!first_seen)
+          {
 	    			first_pose = current_vis_msg.pose;
 	    			first_seen = true;
 	    			mats_arr[0] = getMatFromPose();
-	    		} else {
-	    			Eigen::Matrix4f before = mats_arr[next_tag_id - 1];
-	    			Eigen::Matrix4f Mn = getMatFromPose();
-	    			mats_arr[next_tag_id] = before.inverse() * Mn;
+            cur_tag_id++;
 	    		}
-	    		next_tag_id++;
+          else {
+            /* non first method */
+            if(look_prev && current_vis_msg.id == cur_tag_id-1)
+            {
+              Eigen::Matrix4f prev = getMatFromPose();
+              mats_arr[cur_tag_id] = mats_arr[cur_tag_id-1] * prev.inverse() * mats_arr[cur_tag_id];
+              look_prev = false;
+              cur_tag_id++;
+            }
+            else
+            {
+              /* the case where we see the tag for the first time ever
+              then, we have to get Mn matrix and tell the algo to look back at
+              tag n-1  Tn = Tn-1 * a^-1 * b where a is new perspective of
+              prev tag and b is new tag */
+              Eigen::Matrix4f cur = getMatFromPose();
+              /* temporaily store this matrix into array */
+              mats_arr[cur_tag_id] = cur;
+              look_prev = true;
+            }
+	    		}
+          if(!look_prev) printf("Done calculating %dth tag\n", cur_tag_id-1);
 	    	}
+      marker_seen = false;
 		}
 	}
 }
