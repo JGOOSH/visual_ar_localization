e365101d2ee15bd389e7f6f315a4e7bb24318805
Shivam <patel1.shivam2@gmail.com>
2017-12-09 17:12:29 -0600

    new helper

--- src/chain.cpp
+++ src/chain.cpp
@@ -17,7 +17,8 @@
 bool marker_seen = false;
 bool first_seen = false;
 bool look_prev = false;
-geometry_msgs::Pose first_pose;
+geometry_msgs::PoseStamped first_pose;
+geometry_msgs::PoseStamped sec_pose;
 visualization_msgs::Marker current_vis_msg;
 Eigen::Matrix4f mats_arr[50];
 int cur_tag_id = 0;
@@ -32,15 +33,14 @@ void amcl_cb(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg) {
   robot_pose = *msg;
 }
 
-Eigen::Matrix4f getMatFromPose() {
-  geometry_msgs::Quaternion quat = current_vis_msg.pose.orientation;
+Eigen::Matrix4f getMatFromPose(geometry_msgs::Pose cur_pose) {
+  geometry_msgs::Quaternion quat = cur_pose.pose.orientation;
   Eigen::Quaternionf q(quat.x,quat.y,quat.z,quat.w);
   Eigen::Matrix4f temp;
   temp.block(0, 0, 3, 3) = q.toRotationMatrix();
-  printf ( " sdfsdfsdfsdfsdfsadfasdfads \n");
-  temp(0, 3) = quat.x;
-  temp(1, 3) = quat.y;
-  temp(2, 3) = quat.z;
+  temp(0, 3) = cur_pose.position.x;
+  temp(1, 3) = cur_pose.position.y;
+  temp(2, 3) = cur_pose.position.z;
   temp(3, 3) = 1;
   return temp;
 }
@@ -64,17 +64,23 @@ int main(int argc, char **argv) {
         if(current_vis_msg.id == cur_tag_id) {
           if(!first_seen)
           {
-            first_pose = current_vis_msg.pose;
+            first_pose.header = current_vis_msg.header;
+            first_pose.pose = current_vis_msg.pose;
+            try {
+              tf_l.waitForTransform("/map", first_pose.header.frame_id, ros::Time(0), ros::Duration(1));
+              tf_l.transformPose("/map", first_pose, sec_pose);
+            }
+          catch (tf::TransformException ex) {}
             first_seen = true;
-            mats_arr[0] = getMatFromPose();
-                cur_tag_id++;
-                ROS_INFO("FIRST SEEN");
+            mats_arr[0] = getMatFromPose(sec_pose.pose);
+            cur_tag_id++;
+            ROS_INFO("FIRST SEEN");
           }
           else {
             /* non first method */
             if(look_prev && current_vis_msg.id == cur_tag_id-1)
             {
-              Eigen::Matrix4f prev = getMatFromPose();
+              Eigen::Matrix4f prev = getMatFromPose(current_vis_msg.pose);
               Eigen::Matrix4f temp = mats_arr[cur_tag_id-1] * prev.inverse();
               mats_arr[cur_tag_id] = temp * mats_arr[cur_tag_id];
               printf("%d, %d, %f, %f, %f\n", 1, cur_tag_id, temp(0, 3), temp(1, 3), temp(2, 3));
@@ -88,7 +94,7 @@ int main(int argc, char **argv) {
               then, we have to get Mn matrix and tell the algo to look back at
               tag n-1  Tn = Tn-1 * a^-1 * b where a is new perspective of
               prev tag and b is new tag */
-              Eigen::Matrix4f cur = getMatFromPose();
+              Eigen::Matrix4f cur = getMatFromPose(current_vis_msg.pose);
               /* temporaily store this matrix into array */
               mats_arr[cur_tag_id] = cur;
               look_prev = true;
